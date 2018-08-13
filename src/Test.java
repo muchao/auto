b102d69664ce71ef19d5053181bbbbb9991be8e0
muchao <chaomou@sohu-inc.com>
2018-08-13 16:39:04 +0800

    add times

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,9 @@
 import java.util.Calendar;
+import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
-        
+        Date d = calendar.getTime();
     }
 }
